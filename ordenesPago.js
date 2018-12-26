3ab149cfd7ca9ccbc44ccffc669ee5f08c9bbaef
Rafa <rafatgonzalez@gmail.com>
2018-12-26 09:38:15 +0100

    Adaptación Schema

--- ordenesPago.js
+++ ordenesPago.js
@@ -1,12 +1,25 @@
 var mongoose = require('mongoose');
 
 var ordenSchema = new mongoose.Schema({
-    name: String,
-    phone: Number
+    idProyecto: String,
+    idFactura: String,
+    idComServicios: String,
+    concepto: String,
+    cantidad: number,
+    beneficiario: string,
+    iban: string
 });
 
 ordenSchema.methods.cleanup = function() {
-    return {name: this.name, phone: this.phone};
+    return {
+        idProyecto: this.idProyecto,
+        idFactura: this.idFactura,
+        idComServicios: this.idComServicios,
+        concepto: this.concepto,
+        cantidad: this.cantidad,
+        beneficiario: this.beneficiario,
+        iban: this.iban
+    };
 }
 
 var OrdenPago = mongoose.model('OrdenPago', ordenSchema);
